4be9fcf8d079d3a4303dc569c255e78e89e17078
hayaman3 <kevin.haya@yahoo.com>
2023-03-13 21:37:13 +0800

    Adjusted Skills component

--- src/components/skills/Frontend.jsx
+++ src/components/skills/Frontend.jsx
@@ -4,40 +4,51 @@ import './skills.css';
 function Frontend() {
   return (
     <div className="skills-content">
-      <h3 className="skills-title">Frontend Developer</h3>
+      <h3 className="skills-title">Frontend Developement</h3>
       <div className="skills-box">
-        <div className="skills-data">
-          <i className="devicon-html5-plain" />
-          <div>
-            <h3 className="skill-name">HTML</h3>
+        <div className="skills-group">
+          <div className="skills-data">
+            <i className="devicon-html5-plain" />
+            <div>
+              <h3 className="skills-name">HTML</h3>
+            </div>
+          </div>
+
+          <div className="skills-data">
+            <i className="devicon-css3-plain" />
+            <div>
+              <h3 className="skills-name">CSS</h3>
+            </div>
+          </div>
+
+          <div className="skills-data">
+            <i className="devicon-javascript-plain" />
+            <div>
+              <h3 className="skills-name">JavaScript</h3>
+            </div>
           </div>
         </div>
-
-        <div className="skills-data">
-          <i className="devicon-css3-plain" />
-          <div>
-            <h3 className="skill-name">CSS</h3>
+        <div className="skills-group">
+          <div className="skills-data">
+            <i className="devicon-git-plain" />
+            <div>
+              <h3 className="skills-name">Git</h3>
+            </div>
           </div>
-        </div>
 
-        <div className="skills-data">
-          <i className="devicon-javascript-plain" />
-          <div>
-            <h3 className="skill-name">JavaScript</h3>
+          <div className="skills-data">
+            <i className="devicon-react-original" />
+            <div>
+              <h3 className="skills-name">React</h3>
+            </div>
           </div>
-        </div>
 
-        <div className="skills-data">
-          <i className="devicon-git-plain" />
-          <div>
-            <h3 className="skill-name">Git</h3>
-          </div>
-        </div>
+          <div className="skills-data">
+            <i className="devicon-webpack-plain" />
 
-        <div className="skills-data">
-          <i className="devicon-react-original" />
-          <div>
-            <h3 className="skill-name">React</h3>
+            <div>
+              <h3 className="skills-name">Webpack</h3>
+            </div>
           </div>
         </div>
       </div>
--- src/components/skills/OtherSkills.jsx
+++ src/components/skills/OtherSkills.jsx
@@ -6,43 +6,54 @@ function Other() {
     <div className="skills-content">
       <h3 className="skills-title">Other Languages</h3>
       <div className="skills-box">
-        <div className="skills-data">
-          <i className="devicon-c-plain" />
-          <div>
-            <h3 className="skill-name">C programming</h3>
-            <span className="skill-level">Basic</span>
+        <div className="skills-group">
+          <div className="skills-data">
+            <i className="devicon-c-plain" />
+            <div className="skills-tech">
+              <h3 className="skills-name">C</h3>
+              <span className="skills-level">Basic</span>
+            </div>
+          </div>
+
+          <div className="skills-data">
+            <i className="devicon-java-plain" />
+            <div className="skills-tech">
+              <h3 className="skills-name">Java</h3>
+              <span className="skills-level">Basic</span>
+            </div>
+          </div>
+
+          <div className="skills-data">
+            <i className="devicon-python-plain" />
+            <div className="skills-tech">
+              <h3 className="skills-name">Python</h3>
+              <span className="skills-level">Basic</span>
+            </div>
           </div>
         </div>
-
-        <div className="skills-data">
-          <i className="devicon-java-plain" />
-          <div>
-            <h3 className="skill-name">Java</h3>
-            <span className="skill-level">Basic</span>
+        <div className="skills-group">
+          <div className="skills-data">
+            <i className="devicon-nodejs-plain" />
+            <div className="skills-tech">
+              <h3 className="skills-name">Node.js</h3>
+              <span className="skills-level">Basic</span>
+            </div>
           </div>
-        </div>
 
-        <div className="skills-data">
-          <i className="devicon-python-plain" />
-          <div>
-            <h3 className="skill-name">Python</h3>
-            <span className="skill-level">Basic</span>
+          <div className="skills-data">
+            <i className="devicon-php-plain" />
+            <div className="skills-tech">
+              <h3 className="skills-name">PHP</h3>
+              <span className="skills-level">Basic</span>
+            </div>
           </div>
-        </div>
 
-        <div className="skills-data">
-          <i className="devicon-nodejs-plain" />
-          <div>
-            <h3 className="skill-name">Node.js</h3>
-            <span className="skill-level">Basic</span>
-          </div>
-        </div>
-
-        <div className="skills-data">
-          <i className="devicon-php-plain" />
-          <div>
-            <h3 className="skill-name">PHP</h3>
-            <span className="skill-level">Basic</span>
+          <div className="skills-data">
+            <i className="devicon-cplusplus-plain" />
+            <div className="skills-tech">
+              <h3 className="skills-name">C++</h3>
+              <span className="skills-level">Basic</span>
+            </div>
           </div>
         </div>
       </div>
