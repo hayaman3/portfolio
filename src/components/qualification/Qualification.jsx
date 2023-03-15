0922a6f15d48986fb8cfa54710ccaba67f47e317
hayaman3 <kevin.haya@yahoo.com>
2023-03-15 21:18:09 +0800

    saving progress of Qualification component

--- src/components/qualification/Qualification.jsx
+++ src/components/qualification/Qualification.jsx
@@ -3,9 +3,157 @@ import './qualification.css';
 
 function Qualification() {
   return (
-    <div>
-      <div>Qualification</div>
-    </div>
+    <section className="qualification section">
+      <h2 className="title">Qualification</h2>
+      <span className="section-subtitle">My Journey</span>
+
+      <div className="qualification-container container">
+        <div className="qualification-tabs">
+          <div className="qualification-button button-flex qualification-active">
+            <i className="uil uil-graduation-cap qualification-icon" />
+            Education
+          </div>
+          <div className="qualification-button button-flex">
+            <i className="uil uil-briefcase-alt qualification-icon" />
+            Experience
+          </div>
+        </div>
+
+        <div className="qualification-sections">
+          <div className="qualification-content qualification-content-active">
+            <div className="quaification-data">
+              <div>
+                <h3 className="qualification-title">Title</h3>
+                <span className="qualification-subtitle">
+                  Subtitle
+                </span>
+                <div className="qualification-calendar">
+                  <i className="uil uil-calendar-alt" />
+                  Time
+                </div>
+              </div>
+
+              <div>
+                <span className="qualification-rounder" />
+                <span className="qualification-line" />
+              </div>
+            </div>
+
+            <div className="quaification-data">
+              <div />
+
+              <div>
+                <span className="qualification-rounder" />
+                <span className="qualification-line" />
+              </div>
+              <div>
+                <h3 className="qualification-title">Title</h3>
+                <span className="qualification-subtitle">
+                  Subtitle
+                </span>
+                <div className="qualification-calendar">
+                  <i className="uil uil-calendar-alt" />
+                  Time
+                </div>
+              </div>
+            </div>
+
+            <div className="quaification-data">
+              <div>
+                <h3 className="qualification-title">Title</h3>
+                <span className="qualification-subtitle">
+                  Subtitle
+                </span>
+                <div className="qualification-calendar">
+                  <i className="uil uil-calendar-alt" />
+                  Time
+                </div>
+              </div>
+
+              <div>
+                <span className="qualification-rounder" />
+                <span className="qualification-line" />
+              </div>
+            </div>
+
+            <div className="quaification-data">
+              <div />
+
+              <div>
+                <span className="qualification-rounder" />
+                <span className="qualification-line" />
+              </div>
+              <div>
+                <h3 className="qualification-title">Title</h3>
+                <span className="qualification-subtitle">
+                  Subtitle
+                </span>
+                <div className="qualification-calendar">
+                  <i className="uil uil-calendar-alt" />
+                  Time
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="qualification-content">
+            <div className="quaification-data">
+              <div>
+                <h3 className="qualification-title">Title</h3>
+                <span className="qualification-subtitle">
+                  Subtitle
+                </span>
+                <div className="qualification-calendar">
+                  <i className="uil uil-calendar-alt" />
+                  Time
+                </div>
+              </div>
+
+              <div>
+                <span className="qualification-rounder" />
+                <span className="qualification-line" />
+              </div>
+            </div>
+
+            <div className="quaification-data">
+              <div />
+
+              <div>
+                <span className="qualification-rounder" />
+                <span className="qualification-line" />
+              </div>
+              <div>
+                <h3 className="qualification-title">Title</h3>
+                <span className="qualification-subtitle">
+                  Subtitle
+                </span>
+                <div className="qualification-calendar">
+                  <i className="uil uil-calendar-alt" />
+                  Time
+                </div>
+              </div>
+            </div>
+
+            <div className="quaification-data">
+              <div>
+                <h3 className="qualification-title">Title</h3>
+                <span className="qualification-subtitle">
+                  Subtitle
+                </span>
+                <div className="qualification-calendar">
+                  <i className="uil uil-calendar-alt" />
+                  Time
+                </div>
+              </div>
+
+              <div>
+                <span className="qualification-rounder" />
+                <span className="qualification-line" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
 
