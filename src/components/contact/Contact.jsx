6c4c1ef4aa09e4400913981e085f8617082eb86b
hayaman3 <kevin.haya@yahoo.com>
2023-03-18 22:35:27 +0800

    created Contact component

---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+import React from 'react';
+import './contact.css';
+
+function Contact() {
+  return (
+    <section className="contact-section">
+      <div className="contact-data">contact</div>
+    </section>
+  );
+}
+
+export default Contact;
+
