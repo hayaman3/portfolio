9d9c763e6153c094db11d43d9c890f42243472c9
hayaman3 <kevin.haya@yahoo.com>
2023-03-22 22:36:19 +0800

    added details to Contact component

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -61,7 +61,7 @@ function Contact() {
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                Write me
+                Message me
                 <i
                   className="bx bx-right-arrow-alt contact-button-icon
               "
@@ -79,7 +79,25 @@ function Contact() {
                 rel="noopener noreferrer"
                 className="contact-button"
               >
-                Write me
+                Message me
+                <i
+                  className="bx bx-right-arrow-alt contact-button-icon
+              "
+                />
+              </a>
+            </div>
+
+            <div className="contact-card">
+              <i className="uil uil-map-marker contact-card-icon" />
+              <h3 className="contact-card-title">Location</h3>
+              <span className="contact-card-data">Philippines</span>
+              <a
+                href="https://www.google.com/maps/place/Baguio,+Benguet/@16.3995547,120.55367,13z/data=!3m1!4b1!4m6!3m5!1s0x3391a16879def13f:0x8edef534be3a75c0!8m2!3d16.4023332!4d120.5960071!16zL20vMDE1bDFf"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="contact-button"
+              >
+                My City
                 <i
                   className="bx bx-right-arrow-alt contact-button-icon
               "
