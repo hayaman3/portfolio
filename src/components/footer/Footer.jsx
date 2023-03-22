fca816c336eb348a45c5756011e830a9d5f71283
hayaman3 <kevin.haya@yahoo.com>
2023-03-22 20:12:44 +0800

    created Footer component

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+import React from 'react';
+import './footer.css';
+
+function Footer() {
+  // toggle menu
+
+  return (
+    <footer>
+      asddsad<div>asdsd</div>
+    </footer>
+  );
+}
+
+export default Footer;
+
