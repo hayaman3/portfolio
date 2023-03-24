46b8e41e7ad797f7e1e3abcb324bff5e7b36699f
hayaman3 <kevin.haya@yahoo.com>
2023-03-24 16:18:43 +0800

    completed Scrollup Component

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Skills from './components/skills/Skills';
 import Qualification from './components/qualification/Qualification';
 import Contact from './components/contact/Contact';
 import Footer from './components/footer/Footer';
+import Scrollup from './components/scrollup/Scrollup';
 
 function App() {
   return (
@@ -18,8 +19,9 @@ function App() {
         <Skills />
         <Qualification />
         <Contact />
-        <Footer />
       </main>
+      <Footer />
+      <Scrollup />
     </>
   );
 }
---
+++ src/components/scrollup/Scrollup.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './scrollup.css';
+
+function Scrollup() {
+  window.addEventListener('scroll', () => {
+    const scrollUp = document.querySelector('.scrollup');
+    if (window.scrollY >= 560) {
+      scrollUp.classList.add('show-scroll');
+    } else {
+      scrollUp.classList.remove('show-scroll');
+    }
+  });
+  return (
+    <a href="#a" className="scrollup">
+      <i className="uil uil-arrow-up scrollup-icon" />
+    </a>
+  );
+}
+
+export default Scrollup;
+
